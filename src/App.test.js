293cf20b37e7d025e6bba8677c8957a731ba88d3
samuelyapwoonpin <samuelywp@gmail.com>
2019-11-19 09:40:21 +0800

    Replaced ReactDOM with Enzyme and configured Enzyme with a react-16 adapter

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Enzyme from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+// Configures Enzyme with an adapter.
+// Lets Enzyme know that React 16 is going to be used, and that's what it's
+// going to take as the raw materials to create the virtual DOM.
+Enzyme.configure({adapter: new EnzymeAdapter()})
+
+test('renders without crashing', () => {
+  
 });
